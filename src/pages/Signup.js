9b9152eb814418af0ed159d3961601b281f73b1f
Srishti Jaiswal <84665176+SrishJais@users.noreply.github.com>
2023-04-20 14:49:13 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,7 @@
 // import css file
 import "../css/Signup.css";
 //import from package
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Box, Button, Paper, TextField } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
